99170554f00791cbdfb77484ccf0085995621f0a
Jack <gungravefire@yahoo.com>
2020-06-08 23:19:31 -0500

    first commit: Maintain ping pong in view

---
+++ Assets/PingPongScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongScript : MonoBehaviour
+{
+    void Start()
+    {
+        //Keep ball floating until player can interact with it
+        GetComponent<Rigidbody>().useGravity = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if player is touching ball
+
+
+    }
+}
