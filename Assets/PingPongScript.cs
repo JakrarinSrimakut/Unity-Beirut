a3236ae6fddfe906c3dfae1bd907220187ad8c43
Jack <gungravefire@yahoo.com>
2020-06-09 22:29:33 -0500

    Get touch input of player

--- Assets/PingPongScript.cs
+++ Assets/PingPongScript.cs
@@ -12,8 +12,16 @@ public class PingPongScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Check if player is touching ball
+        //Check if player touching screen
+        if(Input.touchCount > 0)
+        {
+            //Get touch position
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, GetComponent<Transform>().position.z));
 
+            //Check if player touching ball
+            Debug.Log(touchPos.x + "," + touchPos.y + "," + touchPos.z);
+        }
 
     }
 }
