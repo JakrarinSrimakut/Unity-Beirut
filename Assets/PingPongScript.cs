e4648cd52fa625ab31d64b9cc3e7d18e76f22ef3
Jack <gungravefire@yahoo.com>
2020-06-13 22:21:28 -0500

    Set TODO: Make GM and have it handle instantiate pingPongBall and gameover for 3 misses

--- Assets/PingPongScript.cs
+++ Assets/PingPongScript.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class PingPongScript : MonoBehaviour
 {
-
     private Touch touch;
     private Vector2 startPos, direction;
-    float touchTImeStart, timeInterval; //calculate swipe time to control throw force in z direction
-
+    private float touchTImeStart, timeInterval; //calculate swipe time to control throw force in z direction
+    
     [SerializeField]
     float throwForceInXandY = 1f; //control throw force of x and y direction
 
@@ -42,9 +41,11 @@ public class PingPongScript : MonoBehaviour
                     timeInterval = Time.time - touchTImeStart;
                     direction = touch.position - startPos;
                     //Add force to ball depending on direction, swipe time and throw force
-                    //TODO: Add force to y direction to give a thowing motion
                     GetComponent<Rigidbody>().AddForce(direction.x * throwForceInXandY,direction.y * throwForceInXandY, throwForceInZ/timeInterval);
                     GetComponent<Rigidbody>().useGravity = true;
+                    //pingPongBall = Instantiate(pingPongBall, pingPongBall.transform.position, Quaternion.identity);
+                    //Destroy(gameObject, 3f);
+                    //TODO: Create spawnball script under main camera then instantiate from here
                     break;
             }
         }
