a1e1779c00d7cb75b362ffdf3ce460a8002092e0
Jack <gungravefire@yahoo.com>
2020-06-14 19:09:33 -0500

    Reset ball when thrown after 3 seconds

---
+++ Assets/Scripts/GameSetup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetup : MonoBehaviour
+{
+    public GameObject pingPongBall;
+    public int ballRetryLimit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PingPongScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongScript : MonoBehaviour
+{
+    private Touch touch;
+    private Vector2 startPos, direction;
+    private float touchTImeStart, timeInterval; //calculate swipe time to control throw force in z direction
+    private Vector3 ballStartPos;
+
+    [SerializeField]
+    float throwForceInXandY = 1f; //control throw force of x and y direction
+
+    [SerializeField]
+    float throwForceInZ = 1f; //control throw force of z direction
+
+    void Start()
+    {
+        //Keep ball floating until player can interact with it
+        GetComponent<Rigidbody>().useGravity = false;
+        ballStartPos = GetComponent<Transform>().position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if player touching screen
+        if(Input.touchCount > 0)
+        {
+            //Get touch position
+            touch = Input.GetTouch(0);
+
+            //Check swipe
+            switch (touch.phase)
+            {
+                //Screen touch
+                case TouchPhase.Began:
+                    touchTImeStart = Time.time;
+                    startPos = touch.position;
+                    break;
+                // Release your finger
+                case TouchPhase.Ended:
+                    timeInterval = Time.time - touchTImeStart;
+                    direction = touch.position - startPos;
+                    //Add force to ball depending on direction, swipe time and throw force
+                    GetComponent<Rigidbody>().AddForce(direction.x * throwForceInXandY,direction.y * throwForceInXandY, throwForceInZ/timeInterval);
+                    GetComponent<Rigidbody>().useGravity = true;
+                    Invoke("ResetBall", 3f);
+                    break;
+            }
+        }
+    }
+    void ResetBall()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+        GetComponent<Rigidbody>().angularVelocity = new Vector3(0f, 0f, 0f);
+        GetComponent<Transform>().rotation = Quaternion.Euler(0f,0f,0f);
+        GetComponent<Transform>().position = ballStartPos;
+        GetComponent<Rigidbody>().useGravity = false;
+    }
+}
